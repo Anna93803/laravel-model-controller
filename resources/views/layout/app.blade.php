c0e430a76880403df309e8cb223a8a0f146c2e5e
Anna93803 <anna.tassone93@gmail.com>
2022-07-28 22:29:22 +0200

    add card

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <script src="{{ asset("js/app.js") }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <script src="{{ asset("js/app.js") }}"></script> --}}
     <title>@yield('page_title')</title>
 </head>
 <body>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,20 +1,22 @@
 @extends('layout.app')
 
 @section('page_content')
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <ul class="list-unstyled">
-                    @foreach ($movies as $movie)
-                
-                        <li>{{$movie['title']}}</li>
-                            
-                    @endforeach
-        
-                </ul>
+    <div class="container py-5">
+        <h1 class="text-center">Movies</h1>
 
-            </div>
-        </div>
+        <div class="row row-cols-2 g-2">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card bg-warning" >
+                        <div class="card-body">
+                            <h5 class="card-title fs-3 fw-bold">{{$movie['title']}}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{$movie['original_title']}}</h6>
+                            <p class="card-text">{{$movie['nationality'] . " " . $movie['vote']}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>   
     </div>
 
 @endsection
